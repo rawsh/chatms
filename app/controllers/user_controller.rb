7e8c5e5f026cd476b1937c616e3602b32cdbc972
rawas <rawashbourne@gmail.com>
2016-05-18 04:11:34 +0000

    add all

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,5 @@
 class UserController < ApplicationController
 #before_filter :authenticate_user!
-
   def show
     @messages = Message.all.order("created_at DESC")
     @user = User.find(params[:id])
@@ -10,4 +9,5 @@ class UserController < ApplicationController
         format.xml { render :xml => @user }
     end
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ActiveRecord::Base
 	has_many :messages
 	has_many :comments
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	
+	attr_accessor :login
+	
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+    validates_uniqueness_of :username
+    validates_presence_of :username
+    validates :username, length: { in: 4..20 }
+    
+    def self.find_first_by_auth_conditions(warden_conditions)
+        conditions = warden_conditions.dup
+        if login = conditions.delete(:login)
+          where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+        else
+          where(conditions).first
+        end
+    end  
+
 end
