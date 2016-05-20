98618aef1957cfcae5ae9677a69528a8c019e5bc
rawas <rawashbourne@gmail.com>
2016-05-20 03:34:45 +0000

    akk

--- db/migrate/20150518171435_create_messages.rb
+++ db/migrate/20150518171435_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.string :type
       t.string :title
       t.text :description
 
--- db/migrate/20160519044701_drop_ideas_table.rb
+++
@@ -1,9 +0,0 @@
-class DropIdeasTable < ActiveRecord::Migration
-  def up
-    drop_table :ideas
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
